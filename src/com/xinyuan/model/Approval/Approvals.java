0fce6607bf4af530503df4a41d4066854084163e
isaacselement <isaacselement@gmail.com>
2014-01-04 08:30:31 +0800

    mm is good

--- src/com/xinyuan/model/Approval/Approvals.java
+++ src/com/xinyuan/model/Approval/Approvals.java
@@ -4,11 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
 
 /**
  * User Pending approvals
