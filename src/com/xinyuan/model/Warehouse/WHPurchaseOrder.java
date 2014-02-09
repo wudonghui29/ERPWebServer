51144ff1c8cc63cdc9f6a509d4b252d10a0e3971
kevinyby <405437367@qq.com>
2014-02-09 08:40:44 +0800

    dfff

---
+++ src/com/xinyuan/model/Warehouse/WHPurchaseOrder.java
@@ -0,0 +1,80 @@
+package com.xinyuan.model.Warehouse;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.xinyuan.model.BaseOrder;
+
+@Entity
+@Table
+public class WHPurchaseOrder extends BaseOrder {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String buyNO;           //采购单号
+	private Date purchaseDate;      //进货日期
+	
+	private String company;         //厂   商
+	private String contactPeople;   //联 系 人
+	private String companyTel;      //厂商电话
+	
+	private String payCondition;    //付款条件
+	private String payMode;         //付款方式
+	private float freight;          //运   费
+	public String getBuyNO() {
+		return buyNO;
+	}
+	public void setBuyNO(String buyNO) {
+		this.buyNO = buyNO;
+	}
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getContactPeople() {
+		return contactPeople;
+	}
+	public void setContactPeople(String contactPeople) {
+		this.contactPeople = contactPeople;
+	}
+	public String getCompanyTel() {
+		return companyTel;
+	}
+	public void setCompanyTel(String companyTel) {
+		this.companyTel = companyTel;
+	}
+	public String getPayCondition() {
+		return payCondition;
+	}
+	public void setPayCondition(String payCondition) {
+		this.payCondition = payCondition;
+	}
+	public String getPayMode() {
+		return payMode;
+	}
+	public void setPayMode(String payMode) {
+		this.payMode = payMode;
+	}
+	public float getFreight() {
+		return freight;
+	}
+	public void setFreight(float freight) {
+		this.freight = freight;
+	}
+	
+	
+
+}
