fc18813723fe29102f3704cea8c3117ee2604e99
kevinyby <405437367@qq.com>
2014-05-10 14:39:02 +0800

    mmm

---
+++ src/com/xinyuan/action/command/category/WarehouseCommandApply.java
@@ -0,0 +1,148 @@
+package com.xinyuan.action.command.category;
+
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.util.Iterator;
+import java.util.Set;
+
+import com.modules.Introspector.IntrospectHelper;
+import com.modules.Introspector.ModelIntrospector;
+import com.xinyuan.Util.AppModelsHelper;
+import com.xinyuan.action.command.CommandApply;
+import com.xinyuan.dao.SuperDAO;
+import com.xinyuan.dao.impl.SuperDAOIMP;
+import com.xinyuan.model.Warehouse.WHInventoryCHOrder;
+import com.xinyuan.model.Warehouse.WHInventoryOrder;
+import com.xinyuan.model.Warehouse.WHLendOutBill;
+import com.xinyuan.model.Warehouse.WHLendOutOrder;
+import com.xinyuan.model.Warehouse.WHPurchaseBill;
+import com.xinyuan.model.Warehouse.WHPurchaseOrder;
+import com.xinyuan.model.Warehouse.WHScrapOrder;
+
+public class WarehouseCommandApply extends CommandApply {
+	protected void handleFinalApprovalProcess(SuperDAO dao, Object persistence) throws Exception {
+		super.handleFinalApprovalProcess(dao, persistence);
+		
+		SuperDAOIMP daoImp = (SuperDAOIMP)dao;
+		
+		if (persistence instanceof WHScrapOrder) {
+			WHScrapOrder order = (WHScrapOrder)persistence;
+			String codeValue = order.getProductCode();
+			if (codeValue == null) return;
+			float amount =  order.getAmount();
+			
+			WHInventoryOrder inventoryPO= (WHInventoryOrder)daoImp.getObject(WHInventoryOrder.class, "productCode", codeValue);
+			float IVTotalAmount =  inventoryPO.getTotalAmount();
+			inventoryPO.setTotalAmount(IVTotalAmount - amount);
+			
+		    dao.modify(inventoryPO);
+
+		}
+		
+		if (persistence instanceof WHLendOutOrder) {
+			WHLendOutOrder order = (WHLendOutOrder)persistence;
+			String codeValue = order.getProductCode();
+			if (codeValue == null) return;
+			
+			float lendAmount = order.getLendAmount();
+			
+			WHInventoryOrder inventoryPO= (WHInventoryOrder)daoImp.getObject(WHInventoryOrder.class, "productCode", codeValue);
+
+			float IVLendAmout = inventoryPO.getLendAmount();
+			inventoryPO.setLendAmount(IVLendAmout + lendAmount);
+			
+			dao.modify(inventoryPO);
+			
+		}
+		
+		if (persistence instanceof WHLendOutBill) {
+			WHLendOutBill bill = (WHLendOutBill)persistence;
+			WHLendOutOrder order = AppModelsHelper.getOrderByBill(daoImp, bill);
+			
+			String codeValue = order.getProductCode();
+			if (codeValue == null) return;
+			float returnAmount = bill.getReturnAmount();
+
+			
+			WHInventoryOrder inventoryPO= (WHInventoryOrder)daoImp.getObject(WHInventoryOrder.class, "productCode", codeValue);
+			
+			float IVLendAmout = inventoryPO.getLendAmount();
+			inventoryPO.setLendAmount(IVLendAmout - returnAmount);
+			
+			dao.modify(inventoryPO);
+			
+		}
+		
+		if(persistence instanceof WHPurchaseOrder){
+			WHPurchaseOrder order = (WHPurchaseOrder)persistence;
+			Set<WHPurchaseBill>  bills = order.getWHPurchaseBills();
+			if (bills == null || bills.size() == 0) return;
+			
+			Iterator<WHPurchaseBill> iterable = bills.iterator();
+			while (iterable.hasNext()) {
+				WHPurchaseBill aBill = (WHPurchaseBill) iterable.next();
+				String codeValue = aBill.getProductCode();
+				if (codeValue == null) continue;
+				else {
+					
+					float storageAmount = aBill.getStorageNum();
+					
+					WHInventoryOrder inventoryPO= (WHInventoryOrder)daoImp.getObject(WHInventoryOrder.class, "productCode", codeValue);
+					
+					float IVTotalAmount =  inventoryPO.getTotalAmount();
+					inventoryPO.setTotalAmount(IVTotalAmount + storageAmount);
+					
+					
+					float IVTotal = inventoryPO.getPriceBasicUnit() * IVTotalAmount;
+					float PHTotal = aBill.getStorageNum() * aBill.getStorageUnitPrice();
+					float UnitPrice = (IVTotal+PHTotal)/(IVTotalAmount + storageAmount);
+					inventoryPO.setPriceBasicUnit(UnitPrice);
+
+					dao.modify(inventoryPO);
+					
+				}
+			}
+				
+		}
+		
+		if (persistence instanceof WHInventoryCHOrder) {
+			WHInventoryCHOrder order = (WHInventoryCHOrder)persistence;
+			String codeValue = order.getProductCode_O();
+			WHInventoryOrder inventoryPO= (WHInventoryOrder)daoImp.getObject(WHInventoryOrder.class, "productCode", codeValue); 
+			
+			for (PropertyDescriptor pd : Introspector.getBeanInfo(order.getClass()).getPropertyDescriptors()) {
+				
+				if (pd.getReadMethod() != null && !IntrospectHelper.isClassPropertyName(pd.getName())) {
+					
+					String propertyNameNew = pd.getName() ;
+					if (propertyNameNew.indexOf("_N") >=0 ) 
+					{
+						
+						String propertyNames[] = propertyNameNew.split("_");
+						String originPropertyName = propertyNames[0];
+						
+						
+						Object value = ModelIntrospector.getProperty(order, propertyNameNew);
+						
+						if (value == null) continue; 		// ... null
+						
+						if (value instanceof Number) {
+							Number judgeNumber = (Number)value;
+							if (judgeNumber.floatValue() != -1) {
+								ModelIntrospector.setProperty(inventoryPO, originPropertyName, value);
+							} 
+						}else {
+							ModelIntrospector.setProperty(inventoryPO, originPropertyName, value);
+						}
+						
+					}
+				}
+						
+			}
+						
+			dao.modify(inventoryPO);
+			
+		}
+		
+	}
+}
