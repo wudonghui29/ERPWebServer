440b7af0ad34cd5176625bdb0806b40fa78e60c9
isaacselement <isaacselement@gmail.com>
2014-06-21 09:01:12 +0800

    new version of project

--- src/com/modules/HttpWriter/ResponseWriter.java
+++ src/com/modules/HttpWriter/ResponseWriter.java
@@ -9,20 +9,24 @@ import org.apache.struts2.ServletActionContext;
 
 public class ResponseWriter {
 	
+    
+    public static void write(byte[] data) throws IOException {
+        ServletOutputStream output = getResponse().getOutputStream();
+        output.write(data);
+        output.flush();
+    }
+    
+    
+    public static void close() throws IOException {
+        ServletOutputStream output = getResponse().getOutputStream();
+        output.close();
+    }
+    
+    
+    
 	public static HttpServletResponse getResponse() {
 		return ServletActionContext.getResponse();
 	}
 
-	public static void write(byte[] data) throws IOException {
-		ServletOutputStream output = ServletActionContext.getResponse().getOutputStream();
-		output.write(data);
-		output.flush();
-	}
-	
-	
-	public static void close() throws IOException {
-		ServletOutputStream output = ServletActionContext.getResponse().getOutputStream();
-		output.close();
-	}
 	
 }
