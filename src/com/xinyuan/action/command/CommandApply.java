f6d6bb81f07339da5cb9fcb17c0c560f7695d292
isaacselement <isaacselement@gmail.com>
2014-06-23 11:16:35 +0800

    fixed apply bug

--- src/com/xinyuan/action/command/CommandApply.java
+++ src/com/xinyuan/action/command/CommandApply.java
@@ -1,12 +1,9 @@
 package com.xinyuan.action.command;
 
-import com.Global.SessionManager;
 import com.xinyuan.Util.AppModelsHelper;
 import com.xinyuan.Util.ApprovalsDAOHelper;
 import com.xinyuan.dao.SuperDAO;
-import com.xinyuan.message.ConfigConstants;
 import com.xinyuan.model.BaseOrder;
-import com.xinyuan.model.User.User;
 
 
 
@@ -18,15 +15,12 @@ public class CommandApply extends CommandModify {
 		
 		// Check and Handle The Final Approval Stuff
 		if (AppModelsHelper.isFinalApproval(appKey, persistence)) {
-			String signinedUser = ((User)SessionManager.get(ConfigConstants.SIGNIN_USER)).getUsername();
-			ApprovalsDAOHelper.deletePendingApprove(signinedUser, persistence);
 			handleFinalApprovalProcess(dao, persistence);
+		} 
 			
-			// Handle The Pending Approvals
-		} else {
-			
-			ApprovalsDAOHelper.handlePendingApprovals(dao, appKey, forwardUser, persistence);
-		}
+		// Handle The Pending Approvals
+		ApprovalsDAOHelper.handlePendingApprovals(dao, appKey, forwardUser, persistence);
+		
 	}
 
 	
