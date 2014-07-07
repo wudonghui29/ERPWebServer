e49c31776efaa012510a439c2820c8e664bb57ee
isaacselement <isaacselement@gmail.com>
2014-07-07 16:33:08 +0800

    new version

--- src/com/xinyuan/Util/QueueHelper.java
+++ src/com/xinyuan/Util/QueueHelper.java
@@ -19,51 +19,45 @@ import com.xinyuan.model.Setting.APPSettings;
 
 public class QueueHelper {
 
-	public static void flushClients() {
+    public static void flushClients() {
 
-		try {
+        try {
 
-			ResponseMessage responseMessage = new ResponseMessage();
-			responseMessage.results = getRefreshDataList();
-			responseMessage.status = ConfigConstants.STATUS_POSITIVE;
-			String json = GsonHelper.getGson().toJson(responseMessage);
-			byte[] data = json.getBytes("UTF-8");
+            ResponseMessage responseMessage = new ResponseMessage();
+            responseMessage.results = getRefreshDataList();
+            responseMessage.status = ConfigConstants.STATUS_POSITIVE;
+            String json = GsonHelper.getGson().toJson(responseMessage);
+            byte[] data = json.getBytes("UTF-8");
 
-			ServletOutputStream outputStream = ServletActionContext.getResponse().getOutputStream();
+            ServletOutputStream outputStream = ServletActionContext.getResponse().getOutputStream();
 
-			outputStream.write(data);
-			outputStream.flush();
+            outputStream.write(data);
+            outputStream.flush();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static List<Object> getRefreshDataList() {
+    public static List<Object> getRefreshDataList() {
 
-		HumanResourceDAO humanResourceDAO = new HumanResourceDAOIMP();
-		List<Object> hrList = humanResourceDAO.getUsersNOPairs();
+        HumanResourceDAO humanResourceDAO = new HumanResourceDAOIMP();
+        List<Object> hrList = humanResourceDAO.getUsersNOPairs();
 
-		BusinessDAO businessDAO = new BusinessDAOIMP();
-		List<Object> bsList = businessDAO.getClientsNOPairs();
+        BusinessDAO businessDAO = new BusinessDAOIMP();
+        List<Object> bsList = businessDAO.getClientsNOPairs();
 
-		SuperDAOIMP dao = new SuperDAOIMP();
+        SuperDAOIMP dao = new SuperDAOIMP();
 
-		String appSettingTableString = IntrospectHelper
-				.getShortClassName(APPSettings.class);
-		List<Object> settingList = dao
-				.getObjects("select settings.settings from "
-						+ appSettingTableString
-						+ " settings where settings.type = '"
-						+ ConfigConstants.APPSettings_TYPE_ADMIN_APPROVALS
-						+ "'");
+        String appSettingTableString = IntrospectHelper.getShortClassName(APPSettings.class);
+        List<Object> settingList = dao.getObjects("select settings.settings from " + appSettingTableString + " settings where settings.type = '" + ConfigConstants.APPSettings_TYPE_ADMIN_APPROVALS + "'");
 
-		List<Object> results = new ArrayList<Object>();
-		results.add(hrList);
-		results.add(bsList);
-		results.add(settingList);
+        List<Object> results = new ArrayList<Object>();
+        results.add(hrList);
+        results.add(bsList);
+        results.add(settingList);
 
-		return results;
-	}
+        return results;
+    }
 
 }
